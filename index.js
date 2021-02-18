b2fb029620e9a1e635fb4f66edb70f97c53a8bc1
Jodi Croft <jodicroft@jodis-mbp.lan>
2021-02-18 17:55:16 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from home page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
